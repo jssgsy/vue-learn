4f327afbdf6acf3ea286929dc3ea1886c642f546
univ <fcsnwu@163.com>
2025-05-08 11:18:55 +0800

    开发服务器配置补充

--- vite.config.ts
+++ vite.config.ts
@@ -11,11 +11,17 @@ export default defineConfig({
     },
   },
   server: {
+    // 开发环境可解决跨域问题
     proxy: {
-      "/api": { // 此时前端请求接口为：localhost:5173/api/，注意端口是开发服务器的端口
-        target: "http://127.0.0.1:8095", //
+      // 此时前端请求接口须为：http://localhost:5173/api/，必须带schema(http)，注意端口是开发服务器的端口
+      "/api": {
+        target: "http://127.0.0.1:9010", //
         changeOrigin: true, // 统一加上
-        rewrite: (path) => path.replace(/^\/api/, '') // 去掉路径中的 `/api` 前缀
+        rewrite: (path) => {
+          // path是/api及后面的一串(即已经去除了开发服务器的ip及端口号)，如：/api/param/noParam
+          return path.replace(/^\/api/, '') // 去掉路径中的 `/api` 前缀
+        }
+        // 此时请求http://localhost:5173/api/xxx将被转发到http://127.0.0.1:9010/xxx
       }
     }
   }
